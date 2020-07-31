532d9723628c7cd536cd3bc5f03f174e35ed1225
Nenad Marinkovic <nenadm@popwebdesign.net>
2020-07-31 21:22:46 +0200

    initial setup, basic auth

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Home from "./components/Home"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:7575"
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")))
+
+  return (
+    <BrowserRouter>
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <Switch>
+        <Route path="/" exact>
+          {loggedIn ? <Home /> : <HomeGuest />}
+        </Route>
+        <Route path="/create-post">
+          <CreatePost />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
