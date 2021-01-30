d1974a368cb3e4c074bb54da396698372ce96d75
Nenad <nenadmarinkovic@protonmail.com>
2021-01-30 16:42:20 +0100

    initial setup live

--- src/components/About.js
+++ src/components/About.js
@@ -9,11 +9,11 @@ function About() {
         className="lead text-muted"
         style={{ marginTop: "20px", lineHeight: "1.8" }}
       >
-        Notes Café is small, private not-for-profit social network, but open to
-        all who want to use it or contribute to its growth. Users can add notes,
-        follow other users, share knowledge, resources, ideas. There are no
+        Notes Café is a small, private not-for-profit social network, but open
+        to all who want to use it or contribute to its growth. Users can add
+        notes, follow other users, share knowledge and resources. There are no
         distractions, trolls, marketing campaigns, political propaganda, bots,
-        not even your aunt. Just group of friends who share ideas and
+        not even your aunt. Just a group of friends who share ideas and
         interesting links.
       </p>
       <p style={{ marginTop: "20px", lineHeight: "1.8" }}>
